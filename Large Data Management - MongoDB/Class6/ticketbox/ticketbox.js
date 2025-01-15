2f179faeb057daeabb909d96e03cfc42d911b1e7
PhongN18 <chelsea2009021808@gmail.com>
2025-01-15 23:34:13 +0700

    generate data for ticket box assignment + views done

--- Large Data Management - MongoDB/Class6/ticketbox/ticketbox.js
+++ Large Data Management - MongoDB/Class6/ticketbox/ticketbox.js
@@ -21,52 +21,133 @@ db.createView(
 )
 
 
-db.tickets.aggregate([
-    {
-        $lookup: {
-            from: "events",
-            localField: "event_id",
-            foreignField: "_id",
-            as: "event_details"
-        }
-    },
-    { $unwind: "$event_details" },
-    {
-        $addFields: {
-            tier_price: {
-                // get the first element from the filtered array -> get the right tier
-                // $arrayElemAt: [ <array>, <index> ] -> get the <index> element from the <array>
-                $arrayElemAt: [{
-                    // $filter: create a new array containing only the elements that match the 'cond' condition
-                    $filter: {
-                        input: "$event_details.ticket_tiers",
-                        as: "tier",
-                        cond: { $eq: ["$$tier.tier", "$ticket_tier"] }
-                    }
-                }, 0]
+// Events revenue
+db.createView(
+    "event_revenue",
+    "tickets",
+    [
+        {
+            $lookup: {
+                from: "events",
+                localField: "event_id",
+                foreignField: "_id",
+                as: "event_details"
+            }
+        },
+        { $unwind: "$event_details" },
+        {
+            $addFields: {
+                tier_price: {
+                    // get the first element from the filtered array -> get the right tier
+                    // $arrayElemAt: [ <array>, <index> ] -> get the <index> element from the <array>
+                    $arrayElemAt: [{
+                        // $filter: create a new array containing only the elements that match the 'cond' condition
+                        $filter: {
+                            input: "$event_details.ticket_tiers",
+                            as: "tier",
+                            cond: { $eq: ["$$tier.tier", "$ticket_tier"] }
+                        }
+                    }, 0]
+                }
+            }
+        },
+        {
+            $group: {
+                _id: "$event_id",
+                total_revenue: { $sum: "$tier_price.price" },
+                tickets_sold: { $sum: 1 }
+            }
+        },
+        {
+            $lookup: {
+                from: "events",
+                localField: "_id",
+                foreignField: "_id",
+                as: "event_details"
+            }
+        },
+        {
+            $project: {
+                _id: 0,
+                event_name: { $arrayElemAt: ["$event_details.event_name", 0] },
+                total_revenue: "$total_revenue",
+                            tickets_sold: "$tickets_sold"
+            }
+        },
+        { $sort: { total_revenue: -1 } }
+    ]
+);
+
+
+// Customer purchase
+db.createView(
+    "customer_purchases",
+    "customers",
+    [
+        {
+            $lookup: {
+                from: "tickets",
+                localField: "purchased_tickets",
+                foreignField: "_id",
+                as: "ticket_details"
+            }
+        },
+        { $unwind: "$ticket_details" },
+        {
+            $lookup: {
+                from: "events",
+                localField: "ticket_details.event_id",
+                foreignField: "_id",
+                as: "event_details"
+            }
+        },
+        { $unwind: "$event_details" },
+        {
+            $project: {
+                customer_name: "$name",
+                email: 1,
+                event_name: "$event_details.event_name",
+                ticket_tier: "$ticket_details.ticket_tier",
+                purchase_date: "$ticket_details.purchase_date"
             }
         }
-    },
-    {
-        $group: {
-            _id: "$event_id",
-            total_revenue: { $sum: "$tier_price.price" }
-        }
-    },
-    {
-        $lookup: {
-            from: "events",
-            localField: "_id",
-            foreignField: "_id",
-            as: "event_details"
-        }
-    },
-    {
-        $project: {
-            _id: 0,
-            event_name: { $arrayElemAt: ["$event_details.event_name", 0] },
-            total_revenue: "$total_revenue"
-        }
-    },
-    { $sort: { total_revenue: -1 } }
-]);
+    ]
+)
+
+// Tickets sold by each event type
+db.createView(
+    "event_type_sold",
+    "tickets",
+    [
+        {
+            $lookup: {
+                from: "events",
+                localField: "event_id",
+                foreignField: "_id",
+                as: "event_details"
+            }
+        },
+        { $unwind: "$event_details" },
+        {
+            $addFields: {
+                tier_price: {
+                    $arrayElemAt: [{
+                        $filter: {
+                            input: "$event_details.ticket_tiers",
+                            as: "tier",
+                            cond: { $eq: ["$$tier.tier", "$ticket_tier"] }
+                        }
+                    }, 0]
+                }
+            }
+        },
+        {
+            $group: {
+                _id: "$event_details.event_type",
+                tickets_sold: { $sum: 1 },
+                total_revenue: { $sum: "$tier_price.price" }
+            }
+        },
+        { $sort: { tickets_sold: -1, total_revenue: -1 } }
+    ]
+)
